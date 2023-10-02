a9ba35a0db8969eef68b48731e84d8a6c05e3dc0
Andreas <andreas_marquardt@outlook.com>
2023-10-02 17:35:37 +0200

    initial registerfile

--- registerfile.vhdl
+++ registerfile.vhdl
@@ -15,5 +15,17 @@ Port ( I_clock : in  STD_LOGIC;
 end register_file;
 
 architecture Behavioral of register_file is
+	type store_t is array (0 to 31) of std_logic_vector(31 downto 0);
+	signal registers: store_t := (others => X"00000000");
 begin
+	process(I_clock)
+	begin
+		if rising_edge(I_clock) and I_enable = '1' then
+			O_dataA <= registers(to_integer(unsigned(I_selectA)));
+			O_dataB <= registers(to_integer(unsigned(I_selectB)));
+			if (I_write_enable = '1') and (unsigned(I_selectDest) > 0) then
+				registers(to_integer(unsigned(I_selectDest))) <= I_dataDest;
+			end if;
+		end if;
+	end process;
 end;
