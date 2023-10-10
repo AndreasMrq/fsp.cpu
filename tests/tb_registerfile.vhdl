6a7ce407676e5c6c1074aa26fb963786dea3e274
Andreas <andreas_marquardt@outlook.com>
2023-10-10 18:02:06 +0200

    added first real tests

--- tests/tb_registerfile.vhdl
+++ tests/tb_registerfile.vhdl
@@ -26,39 +26,91 @@ architecture tb of tb_registerfile is
 begin
 	registerfile:	entity	work.register_file
 	port map (
-	i_clock=>i_clock,
-	i_enable=>i_enable,
-	i_datadest=>i_datadest,
-	o_dataa=>o_dataa,
-	o_datab=>o_datab,
-	i_selecta=>i_selecta,
-	i_selectb=>i_selectb,
-	i_selectdest=>i_selectdest,
-	i_write_enable=>i_write_enable
-	);
+			 i_clock=>i_clock,
+			 i_enable=>i_enable,
+			 i_datadest=>i_datadest,
+			 o_dataa=>o_dataa,
+			 o_datab=>o_datab,
+			 i_selecta=>i_selecta,
+			 i_selectb=>i_selectb,
+			 i_selectdest=>i_selectdest,
+			 i_write_enable=>i_write_enable
+		 );
+	I_clk_process : process
+	begin
+		i_clock <= '0';
+		wait for I_clk_period/2;
+		i_clock <= '1';
+		wait for I_clk_period/2;
+	end process;
 
-  test_runner : process
-  begin
-    test_runner_setup(runner, runner_cfg);
+	test_runner : process
+	begin
+		test_runner_setup(runner, runner_cfg);
 
-    -- Put test suite setup code here
+		-- Put test suite setup code here
 
-    while test_suite loop
+		while test_suite loop
 
-      -- Put common test case setup code here
+			-- Put common test case setup code here
 
-      if run("Test to_string for integer") then
-        check_equal(to_string(17), "17");
-      elsif run("Test to_string for boolean") then
-        check_equal(to_string(true), "true");
-      end if;
+			if run("Registers are initialized to 0x0") then
+				i_enable <= '1';
+				i_selecta <= "000001";
+				wait for i_clk_period;
+				check_equal(unsigned(o_dataa),0);
+			elsif run("Read Write test") then
+				i_enable <= '1';
 
-      -- Put common test case cleanup code here
+				i_selectdest <= "000001";
+				i_datadest <= x"aaaaaaaa";
+				i_write_enable<='1';
+				wait for i_clk_period;
 
-    end loop;
+				i_selectdest <= "000000";
+				i_datadest <= (others => '0');
+				i_write_enable<='0';
+				i_selecta <= "000001";
+				wait for i_clk_period;
+
+				check_equal(to_string(o_dataa),x"aaaaaaaa");
+			elsif run("No Write for when wirte_enable not asserted") then
+				i_enable <= '1';
+
+				i_selectdest <= "000001";
+				i_datadest <= x"aaaaaaaa";
+				i_write_enable<='0';
+				wait for i_clk_period;
+
+				i_selectdest <= "000000";
+				i_datadest <= (others => '0');
+				i_write_enable<='0';
+				i_selecta <= "000001";
+				wait for i_clk_period;
+
+				check_equal(to_string(o_dataa),x"000000000");
+			elsif run("Register 0 cannot be overriden") then
+				i_enable <= '1';
+
+				i_selectdest <= "000000";
+				i_datadest <= x"aaaaaaaa";
+				i_write_enable<='1';
+				wait for i_clk_period;
+
+				i_datadest <= (others => '0');
+				i_write_enable<='0';
+				i_selecta <= "000000";
+				wait for i_clk_period;
+
+				check_equal(to_string(o_dataa),x"00000000");
+			end if;
+
+    -- Put common test case cleanup code here
+
+		end loop;
 
     -- Put test suite cleanup code here
 
-    test_runner_cleanup(runner);
-  end process;
+		test_runner_cleanup(runner);
+	end process;
 end architecture;
