7c427910593c2f30b32c21752e633d6aef764503
Andreas <andreas_marquardt@outlook.com>
2023-10-17 17:56:21 +0200

    add more decoder stuff

--- src/constants.vhdl
+++ src/constants.vhdl
@@ -1,7 +1,41 @@
 library ieee;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
-entity constants is
+package constants is
+	-- Instruction Form Offsets
+	constant OPCODE_START: integer := 6;
+	constant OPCODE_END: integer := 0;
+	constant OPCODE_END_2: integer := 2;
+
+	constant RD_START: integer := 11;
+	constant RD_END: integer := 7;
+
+	constant FUNCT3_START: integer := 14;
+	constant FUNCT3_END: integer := 12;
+
+	constant R1_START: integer := 19;
+	constant R1_END: integer := 15;
+
+	constant R2_START: integer := 24;
+	constant R2_END: integer := 20;
+
+	constant FUNCT7_START: integer := 31;
+	constant FUNCT7_END: integer := 25;
+
+	constant IMM_I_START: integer := 31;
+	constant IMM_I_END: integer := 20;
+
+	constant IMM_U_START: integer := 31;
+	constant IMM_U_END: integer := 12;
+
+	constant IMM_S_A_START: integer := 31;
+	constant IMM_S_A_END: integer := 25;
+
+	constant IMM_S_B_START: integer := 11;
+	constant IMM_S_B_END: integer := 7;
+
+
+	-- OP Codes
 	constant OP_LUI : std_logic_vector (6 downto 0) := "0110111";
 	constant OP_AUIPC : std_logic_vector (6 downto 0) := "0010111";
 	constant OP_JAL : std_logic_vector (6 downto 0) := "1101111";
@@ -14,18 +48,67 @@ entity constants is
 	constant OP_FENCE : std_logic_vector (6 downto 0) := "0001111";
 	constant OP_ENV : std_logic_vector (6 downto 0) := "1110011";
 
-	constant FUN_BEQ : std_logic_vector (2 downto 0) := "000";
-	constant FUN_BNE : std_logic_vector (2 downto 0) := "001";
-	constant FUN_BLT : std_logic_vector (2 downto 0) := "100";
-	constant FUN_BGE : std_logic_vector (2 downto 0) := "101";
-	constant FUN_BLTU : std_logic_vector (2 downto 0) := "110";
-	constant FUN_BGEU : std_logic_vector (2 downto 0) := "111";
-	constant FUN_LB : std_logic_vector (2 downto 0) := "000";
-	constant FUN_LH : std_logic_vector (2 downto 0) := "001";
-	constant FUN_LW : std_logic_vector (2 downto 0) := "010";
-	constant FUN_LBU : std_logic_vector (2 downto 0) := "100";
-	constant FUN_LHU : std_logic_vector (2 downto 0) := "101";
-	constant FUN_SB : std_logic_vector (2 downto 0) := "000";
-	constant FUN_SH : std_logic_vector (2 downto 0) := "001";
-	constant FUN_SW : std_logic_vector (2 downto 0) := "010";
+
+	-- Flags
+	constant F3_BRANCH_BEQ: std_logic_vector(2 downto 0) := "000";
+	constant F3_BRANCH_BNE: std_logic_vector(2 downto 0) := "001";
+	constant F3_BRANCH_BLT: std_logic_vector(2 downto 0) := "100";
+	constant F3_BRANCH_BGE: std_logic_vector(2 downto 0) := "101";
+	constant F3_BRANCH_BLTU: std_logic_vector(2 downto 0) := "110";
+	constant F3_BRANCH_BGEU: std_logic_vector(2 downto 0) := "111";
+
+	constant F3_JALR: std_logic_vector(2 downto 0) := "000";
+
+	constant F3_LOAD_LB: std_logic_vector(2 downto 0) := "000";
+	constant F3_LOAD_LH: std_logic_vector(2 downto 0) := "001";
+	constant F3_LOAD_LW: std_logic_vector(2 downto 0) := "010";
+	constant F3_LOAD_LBU: std_logic_vector(2 downto 0) := "100";
+	constant F3_LOAD_LHU: std_logic_vector(2 downto 0) := "101";
+
+	constant F2_MEM_LS_SIZE_B: std_logic_vector(1 downto 0) := "00";
+	constant F2_MEM_LS_SIZE_H: std_logic_vector(1 downto 0) := "01";
+	constant F2_MEM_LS_SIZE_W: std_logic_vector(1 downto 0) := "10";
+
+	constant F3_STORE_SB: std_logic_vector(2 downto 0) := "000";
+	constant F3_STORE_SH: std_logic_vector(2 downto 0) := "001";
+	constant F3_STORE_SW: std_logic_vector(2 downto 0) := "010";
+
+	constant F3_OPIMM_ADDI: std_logic_vector(2 downto 0) := "000";
+	constant F3_OPIMM_SLTI: std_logic_vector(2 downto 0) := "010";
+	constant F3_OPIMM_SLTIU: std_logic_vector(2 downto 0) := "011";
+	constant F3_OPIMM_XORI: std_logic_vector(2 downto 0) := "100";
+	constant F3_OPIMM_ORI: std_logic_vector(2 downto 0) := "110";
+	constant F3_OPIMM_ANDI: std_logic_vector(2 downto 0) := "111";
+
+	constant F3_OPIMM_SLLI: std_logic_vector(2 downto 0) := "001";
+	constant F7_OPIMM_SLLI: std_logic_vector(6 downto 0) := "0000000";
+	constant F3_OPIMM_SRLI: std_logic_vector(2 downto 0) := "101";
+	constant F7_OPIMM_SRLI: std_logic_vector(6 downto 0) := "0000000";
+	constant F3_OPIMM_SRAI: std_logic_vector(2 downto 0) := "101";
+	constant F7_OPIMM_SRAI: std_logic_vector(6 downto 0) := "0100000";
+
+	constant F3_OP_ADD: std_logic_vector(2 downto 0) := "000";
+	constant F7_OP_ADD: std_logic_vector(6 downto 0) := "0000000";
+	constant F3_OP_SUB: std_logic_vector(2 downto 0) := "000";
+	constant F7_OP_SUB: std_logic_vector(6 downto 0) := "0100000";
+	constant F3_OP_SLL: std_logic_vector(2 downto 0) := "001";
+	constant F7_OP_SLL: std_logic_vector(6 downto 0) := "0000000";
+	constant F3_OP_SLT: std_logic_vector(2 downto 0) := "010";
+	constant F7_OP_SLT: std_logic_vector(6 downto 0) := "0000000";
+	constant F3_OP_SLTU: std_logic_vector(2 downto 0) := "011";
+	constant F7_OP_SLTU: std_logic_vector(6 downto 0) := "0000000";
+	constant F3_OP_XOR: std_logic_vector(2 downto 0) := "100";
+	constant F7_OP_XOR: std_logic_vector(6 downto 0) := "0000000";
+	constant F3_OP_SRL: std_logic_vector(2 downto 0) := "101";
+	constant F7_OP_SRL: std_logic_vector(6 downto 0) := "0000000";
+	constant F3_OP_SRA: std_logic_vector(2 downto 0) := "101";
+	constant F7_OP_SRA: std_logic_vector(6 downto 0) := "0100000";
+	constant F3_OP_OR: std_logic_vector(2 downto 0) := "110";
+	constant F7_OP_OR: std_logic_vector(6 downto 0) := "0000000";
+	constant F3_OP_AND: std_logic_vector(2 downto 0) := "111";
+	constant F7_OP_AND: std_logic_vector(6 downto 0) := "0000000";
+end constants;
+
+package body constants is
+ 
 end constants;
--- src/decoder.vhdl
+++ src/decoder.vhdl
@@ -1,6 +1,8 @@
 library ieee;
+library work;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
+use work.constants.all;
 
 entity decoder is
     port ( i_clock : in  std_logic;
@@ -11,32 +13,40 @@ entity decoder is
            o_selectdest : out  std_logic_vector (4 downto 0);
            o_data_imm : out  std_logic_vector (31 downto 0);
            o_write_enable : out  std_logic;
-           o_opcode : out  std_logic_vector (6 downto 0));
+           o_opcode : out  std_logic_vector (6 downto 0);
+    	   o_function: out std_logic_vector (9 downto 0)
+    );
 end decoder;
 
 architecture behavioral of decoder is
 
-begin
+constant ZERO : std_logic_vector(31 downto 0) := (others => '0');
 
+begin
   process (i_clock)
   begin
     if rising_edge(i_clock) and i_enable = '1' then
 
-      o_opcode <= i_data_instruction(6 downto 0);
-      o_selectdest <= i_data_instruction(11 downto 7);
-      o_selecta <= i_data_instruction(19 downto 15);
-      o_selectb <= i_data_instruction(24 downto 20);
+      o_opcode <= i_data_instruction(OPCODE_START downto OPCODE_END);
+      o_selectdest <= i_data_instruction(RD_START downto RD_END);
+      o_selecta <= i_data_instruction(R1_START downto R1_END);
+      o_selectb <= i_data_instruction(R2_START downto R2_END);
 
-      o_data_imm <= i_data_instruction(7 downto 0) & i_data_instruction(7 downto 0);
-      o_opcode <= i_data_instruction(15 downto 12) & i_data_instruction(8);
-
-      case i_data_instruction(15 downto 12) is
-        when "0111" => 	-- write
-          o_write_enable <= '0';
-        when "1100" => 	-- jump
-          o_write_enable <= '0';
-        when "1101" => 	-- jumpeq
-          o_write_enable <= '0';
+      case i_data_instruction(OPCODE_START downto OPCODE_END) is
+        when OP_LUI => 
+	  o_data_imm <= i_data_instruction(31 downto 12) & ZERO(11 downto 0);
+        when OP_AUIPC => 
+	  o_data_imm <= i_data_instruction(31 downto 12) & ZERO(11 downto 0);
+ 	when OP_JAL => 
+	  o_data_imm <= ZERO(31 downto 21) & i_data_instruction(31) & i_data_instruction(19 downto 12) & i_data_instruction(20) & i_data_instruction(30 downto 21) & ZERO(0);
+        when OP_JALR => 
+	  o_data_imm <= ZERO(31 downto 12) & i_data_instruction(31 downto 20);
+        when OP_BRANCH =>
+	  o_data_imm <= ZERO(31 downto 13) & i_data_instruction(31) & i_data_instruction(7) & i_data_instruction(30 downto 25) & i_data_instruction(11 downto 8) & ZERO(0);
+        when OP_LOAD =>
+	  o_data_imm <= ZERO(31 downto 12) & i_data_instruction(31 downto 20);
+        when OP_STORE =>
+	  o_data_imm <= ZERO(31 downto 12) & i_data_instruction(31 downto 25) & i_data_instruction(11 downto 7);
         when others =>
           o_write_enable <= '1';
       end case;
